f97d9ca71f1385070c954540bc40751c6e510b3d
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-02-20 15:21:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,7 @@
 
 ##This method takes in an array of numbers and outputs that array with all the numbers squared
 def square_array(array)
-  return array.collect {|numb| numb**2}
+  new_array = array.collect {|numb| numb**2}
+
+  return new_array
 end
